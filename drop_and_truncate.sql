2d96873e8722428945a9d051cf4e009915148c89
Aravind Ganipisetty <aravindganipisetty@gmail.com>
2025-12-27 12:35:33 -0500

    Clean up SQL script and fix syntax issues
    Removed unnecessary MySQL monitor output and corrected SQL syntax errors.

--- drop_and_truncate.sql
+++ drop_and_truncate.sql
@@ -1,16 +1,3 @@
-Enter password: ************
-Welcome to the MySQL monitor.  Commands end with ; or \g.
-Your MySQL connection id is 18
-Server version: 9.5.0 MySQL Community Server - GPL
-
-Copyright (c) 2000, 2025, Oracle and/or its affiliates.
-
-Oracle is a registered trademark of Oracle Corporation and/or its
-affiliates. Other names may be trademarks of their respective
-owners.
-
-Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
-
 mysql> create database drop_turncate_example;
 Query OK, 1 row affected (0.145 sec)
 
@@ -60,17 +47,6 @@ ERROR 1146 (42S02): Table 'drop_turncate_example.data' doesn't exist
 mysql> show tables;
 Empty set (0.023 sec)
 
-mysql>  create table data(
-    ->     -> sid int,
-    ->     -> sname varchar(30),
-    ->     -> perc float);
-ERROR 1064 (42000): You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '-> sid int,
-    -> sname varchar(30),
-    -> perc float)' at line 2
-mysql>  create table data(
-    ->  sid int,
-    ->  sname varchar(30);
-ERROR 1064 (42000): You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '' at line 3
 mysql> CREATE TABLE data (
     ->     sid INT,
     ->     sname VARCHAR(30)
@@ -88,19 +64,7 @@ mysql> show tables;
 mysql> insert into data values( 1, "aravind" ,99 ),
     -> (2, "gani", 98);
 ERROR 1136 (21S01): Column count doesn't match value count at row 1
-mysql> insert into data values (1, "aravind" ,99),
-    ->
-    ->
-    ->
-    ->
-    ->
-    ->
-    ->
-    ->
-    ->
-    -> (2, "gani" ,90
-    -> );
-ERROR 1136 (21S01): Column count doesn't match value count at row 1
+    
 mysql> INSERT INTO data VALUES
     -> (1, 'aravind'),
     -> (2, 'gani');
@@ -140,3 +104,4 @@ mysql> select * from data;
 Empty set (0.015 sec)
 
 mysql>
+
